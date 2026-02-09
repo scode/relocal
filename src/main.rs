83dc28892757f107b9dc20a6ae20f5a64e6d07c0
Peter Schuller <peter.schuller@infidyne.com>
2026-02-08 18:02:50 -0800

    Step 2b.

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,30 @@ mod discovery;
 mod error;
 mod session;
 
-fn main() {}
+use clap::Parser;
+use cli::{Cli, Command, RemoteCommand, SyncCommand};
+use tracing_subscriber::FmtSubscriber;
+
+fn main() {
+    let cli = Cli::parse();
+
+    FmtSubscriber::builder()
+        .with_max_level(cli.log_level())
+        .init();
+
+    match cli.command {
+        Command::Init => eprintln!("not yet implemented: init"),
+        Command::Remote { command } => match command {
+            RemoteCommand::Install => eprintln!("not yet implemented: remote install"),
+            RemoteCommand::Nuke => eprintln!("not yet implemented: remote nuke"),
+        },
+        Command::Start { .. } => eprintln!("not yet implemented: start"),
+        Command::Sync { command } => match command {
+            SyncCommand::Push { .. } => eprintln!("not yet implemented: sync push"),
+            SyncCommand::Pull { .. } => eprintln!("not yet implemented: sync pull"),
+        },
+        Command::Status { .. } => eprintln!("not yet implemented: status"),
+        Command::List => eprintln!("not yet implemented: list"),
+        Command::Destroy { .. } => eprintln!("not yet implemented: destroy"),
+    }
+}
